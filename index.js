b2c8ac9afccd7897a275593dc1315d7f5cb451b4
Sarah Whalley <s.whalley1130@gmail.com>
2023-01-30 16:38:00 -0700

    Completes lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(val)
+{
+    return Math.abs(val-42);
+}
+
+function distanceFromHqInFeet(val)
+{
+    return (distanceFromHqInBlocks(val)*264);
+}
+
+function distanceTravelledInFeet(start, end)
+{
+    return (Math.abs(start-end)*264);
+}
+
+function calculatesFarePrice(start, destination)
+{
+    const distance=distanceTravelledInFeet(start, destination);
+    console.log(distance);
+    if (distance<400)
+    {
+        return 0;
+    }
+    else if ((400<=distance) && (distance<=2000))
+    {
+        return ((distance-400)*0.02);
+    }
+    else if ((2000<=distance) && (distance<=2500)) 
+    {
+        return 25;
+    }
+    else 
+    {
+        return 'cannot travel that far';
+    }
+}
